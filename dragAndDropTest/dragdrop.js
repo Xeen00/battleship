4a0d72166bb36b3e1b1b623a0859dc2fa60aeec8
xenoisenegger <xeno.isenegger@students.fhnw.ch>
2024-06-08 17:16:44 +0200

    testing to being able to make dragImg transparent on hover to have a nice looking snap effect

--- dragAndDropTest/dragdrop.js
+++ dragAndDropTest/dragdrop.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const draggable = document.getElementById('draggable');
-
+    const dropzones = document.getElementsByClassName('dropzone');
     let dragImg;
 
+
+
     draggable.addEventListener('dragstart', (e) => {
-        dragImg = createDragImage("Initial Text");
+        dragImg = createDragImage("");
         document.body.appendChild(dragImg);
         e.dataTransfer.setDragImage(dragImg, 50, 50);
     });
@@ -24,11 +26,33 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    for (let dropzone of dropzones) {
+        dropzone.addEventListener('dragover', (e) => {
+            e.preventDefault();
+            dragImg.draggable.style.opacity = 0;
+            dropzone.style.borderColor = 'green';
+        });
+
+        dropzone.addEventListener('dragleave', (e) => {
+            dropzone.style.borderColor = 'grey';
+            draggable.style.opacity = 1;
+        });
+
+        dropzone.addEventListener('drop', (e) => {
+            e.preventDefault();
+            if (dragImg) {
+                document.body.removeChild(dragImg);
+            }
+            dropzone.appendChild(draggable);
+            dropzone.style.borderColor = 'grey';
+        });
+    }
+
     function createDragImage(text) {
         const img = document.createElement('div');
         img.style.width = '100px';
         img.style.height = '100px';
-        img.style.backgroundColor = 'red';
+        img.style.backgroundColor = 'blue';
         img.innerHTML = text;
         img.style.color = 'white';
         img.style.display = 'flex';
